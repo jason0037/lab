17712fd522d6fc4cef1e51d2a9dfe9417851caac
jason <jason0037@21cn.com>
2014-04-22 00:54:23 +0800

    change mongo.db

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-MongoMapper.connection = Mongo::Connection.new('101.226.163.149', 27017)  
+MongoMapper.connection = Mongo::Connection.new('10.13.4.100', 27017)  
 MongoMapper.database = 'cs_comments_service_development' 
   
 if defined?(PhusionPassenger)  
