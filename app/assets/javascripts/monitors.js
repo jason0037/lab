292d0c96d817106cad01e200f4fe786437e7bdb1
sceneryye <scenery.ye@gmail.com>
2014-04-21 12:19:06 +0800

    add muti_chart

--- app/assets/javascripts/monitors.js
+++ app/assets/javascripts/monitors.js
@@ -6,10 +6,16 @@ $(document).ready(function(){
     var realtimeChart = new FusionCharts( "/FusionCharts/RealTimeLineDY.swf", 
                                                   "chart_realtime_01", "100%", 250, "0", "1" );
     //realtimeChart.setXMLUrl("/monitors/camera_data?equipment_code=000001");
-    for( var i=1;i++;i<=4){
-        if($('#data_source_'+i))
-        var data_source = $('#data_source_'+i).val();
-        realtimeChart.setXMLUrl(data_source);
-        realtimeChart.render("chart-container-"+i);
+
+    var tempString =' = new FusionCharts( "/FusionCharts/RealTimeLineDY.swf","chart_realtime_01", "100%", 250, "0", "1" )';
+
+    for( var i=1;i<=4;i++){
+
+        if($('#data_source_'+i)!='undefined'){
+            var data_source = $('#data_source_'+i).val();
+            eval('var rc'+i+tempString);
+            eval('rc'+i+'.setXMLUrl(data_source)');
+            eval('rc'+i+'.render("chart-container-'+i+'")');
+        }
     }
 });
