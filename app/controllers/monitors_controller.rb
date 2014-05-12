6c5440d95aa29ec06e7fc85d41662383aad1c275
sceneryye <scenery.ye@gmail.com>
2014-05-12 16:20:51 +0800

    add charts of energy_consumption

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -41,6 +41,7 @@ class MonitorsController < ApplicationController
         read_at = (Time.now-5.seconds).strftime('%Y%m%d%H%M%S')
         sql = "select sum(value)/4 as value,read_at from #{table_name}_reading where read_at = '#{read_at.to_s}'
         group by read_at order by read_at limit 0,1"
+        
       when 'COOOOO1'
         read_at = (Time.now-5.seconds).strftime('%Y%m%d%H%M%S')
         sql = "select value from #{table_name}_reading where point_id = '#{point_id}'
