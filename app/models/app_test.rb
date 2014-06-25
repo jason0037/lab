8f7e1226753be69ef28d3b02e4669536953c6f8f
sceneryye <scenery.ye@gmail.com>
2014-06-26 05:57:26 +0800

    add interface for APP

--- app/models/app_test.rb
+++ app/models/app_test.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-class AppTests < ActiveRecord::Base
+class AppTest < ActiveRecord::Base
   attr_accessible :user_id, :test_id, :score, :deviceType, :version
 
   belongs_to :lab_user,:foreign_key=>"user_id"
