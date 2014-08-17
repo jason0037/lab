77cfe570f6c555e3a70031bba2c62db2ebae8730
sceneryye <scenery.ye@gmail.com>
2014-08-17 16:30:17 +0800

    fix bugs

--- app/models/lab_course.rb
+++ app/models/lab_course.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class LabCourse < ActiveRecord::Base
   attr_accessible :apply_time, :approve_time, :before, :category_id, :course_property,
-                  :desc, :end_time, :need_teach, :name, :progress, :publish_time, :project_id,
+                  :desc, :end_time, :need_teacher, :name, :progress, :publish_time, :project_id,
                   :start_time, :status, :scene_id, :teacher_id, :course_type,:location, :participants
   belongs_to :lab_eval_project,:foreign_key=>"project_id"
 
