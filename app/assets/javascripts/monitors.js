683e9c4a1eed94e4ee039bde8d8d1c3bc6999e9a
jaychenq <jason0037@163.com>
2014-10-12 19:13:33 +0800

    bugfix

--- app/assets/javascripts/monitors.js
+++ app/assets/javascripts/monitors.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             var chartDiv =  $('#chart-container-'+i);
             var chart_type = chartDiv.attr("chart-type");
             var data_source = chartDiv.attr("data-source");
-            var rc = new FusionCharts( "/FusionCharts/"+chart_type,"chart_new_"+i, "100%", "100%", "0", "1" );
+            var rc = new FusionCharts( "/FusionCharts/"+chart_type,"chart_new_"+i, "100%", "150px", "0", "1" );
             rc.setXMLUrl(data_source);
             rc.render("chart-container-"+i);
         }
